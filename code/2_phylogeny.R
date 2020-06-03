ef0801ffca691701eab3d486941d03d526a3d78d
Eleanor Jackson <eleanor.elizabeth.j@gmail.com>
2020-06-03 15:42:32 +0100

    re fit glmms and add GAMs

--- code/2_phylogeny.R
+++ code/2_phylogeny.R
@@ -18,13 +18,20 @@ theme_set(theme_pubr(base_size = 40))
 fruitDat <- read.csv("../output/tables/cleanData.csv")
 PhyloExtraSpec<-read.tree('../data/PhyloExtraSpec.tree')
 
-# remove outliers
-fruitDat$bcireproductive <- ifelse(fruitDat$bcireproductive > 500, NA, fruitDat$bcireproductive)
+fruitDat %>%
+	mutate(seedpred_pres = factor(ifelse(seedpred_pres == 0, "no", "yes"))) -> fruitDat
 
-fruitDat$cofruit <- ifelse(fruitDat$cofruit < 100, NA, fruitDat$cofruit)
+fruitDat$seed_dry_log <- log10(fruitDat$seed_dry)
+fruitDat$cofruit_log <- log10(fruitDat$cofruit)
+fruitDat$bcireproductive_log <- log10(fruitDat$bcireproductive)
+
+#round these to integers ?
+fruitDat$abscised_seeds <- round(fruitDat$abscised_seeds)
+fruitDat$viable_seeds <- round(fruitDat$viable_seeds)
+fruitDat$total_seeds <- fruitDat$viable_seeds + fruitDat$abscised_seeds
+fruitDat$proportion_abscised <- fruitDat$abscised_seeds/fruitDat$total_seeds
 
 # add taxa name for those that are missing
-
 fruitDat$plant_species19 <- ifelse(is.na(fruitDat$plant_species19), paste(fruitDat$genus, fruitDat$species, sep="_"), paste(fruitDat$plant_species19))
 
 fruitDat$plant_species19 <- as.factor(fruitDat$plant_species19)
@@ -61,90 +68,67 @@ compDat <- comparative.data(phy = phylo, data =as.data.frame(fruitDat),
 compDat$dropped$tips
 compDat$dropped$unmatched.rows
 
-##pgls model, Pagel’s Lambda, using the Maximum Likelihood (ML) estimate of λ
 
-par(mfrow = c(2, 3)) 
+# function to loop through and fit pgls models
 
-# LOCAL ABUNDANCE
-lambdaBR <- caper::pgls(proportion_abscised ~ bcireproductive, data = compDat, lambda = "ML")
-summary(lambdaBR) # lambda is 0.294, p is 4.909e-05 high R of 0.1111
-#OLS model to compare
-lmodel <- lm(proportion_abscised ~ bcireproductive, data = fruitDat)
-# plot the model
-plot(proportion_abscised ~ bcireproductive, data = fruitDat, cex=3, cex.lab=4) + abline(lambdaBR, col="red") + abline(lmodel) +title(main="Local Abundance", cex.main=5)
+vars <- names(fruitDat)[c(13,14,17,21,25:28)]
 
-#check the likelihood profile
-lambda.profile <- pgls.profile(lambdaBR, "lambda")
-plot(lambda.profile) # flat likelihood surface
-#print the confidence intervals
-pgls.confint(model.pgls, "lambda")$ci.val
-
-# SEED DRY MASS
-lambdaSD <- pgls(proportion_abscised ~ log10(seed_dry), data = compDat, lambda = "ML")
-summary(lambdaSD) # lambda is 0.242, p=0.0007574
-
-lambda.profile <- pgls.profile(lambdaSD, "lambda")
-plot(lambda.profile) # also flat
-
-lmodel <- lm(proportion_abscised ~ log10(seed_dry), data = fruitDat)
-plot(proportion_abscised ~ log10(seed_dry), data = fruitDat, cex=3,cex.lab=4) + abline(lambdaSD, col="red") + abline(lmodel) +title(main="Seed dry mass", cex.main=5)
-
-# TREE HEIGHT
-lambdaHA <- pgls(proportion_abscised ~ height_avg, data = compDat, lambda = "ML")
-summary(lambdaHA) # 0.220 p=8.646e-05 this has high R value of 0.1009?
-
-lambda.profile <- pgls.profile(lambdaHA, "lambda")
-plot(lambda.profile) # flat
-
-lmodel <- lm(proportion_abscised ~ height_avg, data = fruitDat)
-plot(proportion_abscised ~ height_avg, data = fruitDat, cex=3,cex.lab=4) + abline(lambdaHA, col="red") + abline(lmodel) +title(main="Tree height", cex.main=5)
+pglsmodels <- lapply(setNames(vars, vars), function(var) {
+	formula = paste("proportion_abscised ~", var)
+	caper::pgls(formula=eval(parse(text=formula)), data=compDat, lambda = "ML")
+	}
+)
 
 
-#  CROP SIZE VARIATION
-lambdaCV <- pgls(proportion_abscised ~ cvseed, data = compDat, lambda = "ML")
-summary(lambdaCV) 
+# plot in loop
 
-lambda.profile <- pgls.profile(lambdaCV, "lambda")
-plot(lambda.profile) # flat!
+pglsmodels$seedpred_pres <- NULL
+pglsmodels$dispersal_mode <- NULL
 
-lmodel <- lm(proportion_abscised ~ cvseed, data = fruitDat)
-plot(proportion_abscised ~ cvseed, data = fruitDat, cex=3,cex.lab=4) + abline(lambdaCV, col="red") + abline(lmodel) +title(main="Crop size variation", cex.main=5)
+labs <- c(
+            height_avg = "Tree height",
+            cvseed = "Interannual crop size variation",
+            cofruit_log = "Overlap in fruit production (log)",
+            bcireproductive_log = "Local abundance (log)",
+            endocarp_investment = "Endocarp investment",
+            seed_dry_log ="Seed dry mass (log)",
+            proportion_abscised = "Proportion of seeds abscised")
 
-# OVERLAP IN FRUIT PRODUCTION
-lambdaCF <- pgls(proportion_abscised ~ log10(cofruit), data = compDat, lambda = "ML")
-summary(lambdaCF) 
+eq <- function(x) predict(pglsmodels$height_avg, newdata=data.frame(height_avg=x))
 
-lambda.profile <- pgls.profile(lambdaCF, "lambda")
-plot(lambda.profile)
+plot_data_var <- function (model, modelname) {
+    ggplot(data= fruitDat, aes_string(x = modelname, y = "proportion_abscised")) +
+        geom_point(size=1) +
+		stat_function(geom="line", colour="blue", fun = eq) +
+        xlab(modelname) +
+        ylab("Proportion of seeds abscised") +
+        xlab(labs[[modelname]]) +
+        theme_bw(base_size=40)
+}
 
-lmodel <- lm(proportion_abscised ~ log10(cofruit), data = fruitDat)
-plot(proportion_abscised ~ log10(cofruit), data = fruitDat, cex=3,cex.lab=4) + abline(lambdaCF, col="red") + abline(lmodel) +title(main="Overlap in fruit production", cex.main=5)
+myplots <- imap(pglsmodels, plot_data_var)
 
-# ENDOCARP INVESTMENT
-lambdaEI <- pgls(proportion_abscised ~ endocarp_investment, data = compDat, lambda = "ML")
-summary(lambdaEI) # 0.286
+ggpubr::ggarrange(plotlist = myplots)
+#ggsave("../output/plots/pgls.png", dpi="screen")
 
-lambda.profile <- pgls.profile(lambdaEI, "lambda")
-plot(lambda.profile) #FLAT!
+#Calculate phylogenetic signal for traits
+row.names(fruitDat) <- fruitDat$taxa
+fd2 = fruitDat[c(3,13,14,17,25:27)]
+sigk<-picante::multiPhylosignal(fd2, phylo)
+write.csv(sig,"../output/tables/multiPhylosignal.csv")
 
-lmodel <- lm(proportion_abscised ~ endocarp_investment, data = fruitDat)
-plot(proportion_abscised ~ endocarp_investment, data = fruitDat, cex=3,cex.lab=4) + abline(lambdaEI, col="red") + abline(lmodel) +title(main="Endocarp investment", cex.main=5) 
-
-par(mfrow = c(1, 1))
-
-# SEED PRED
-lambdaSP <- pgls(proportion_abscised ~ seedpredationrate, data = compDat, lambda = "ML")
-summary(lambdaSP)
-
-lmodel <- lm(proportion_abscised ~ seedpredationrate, data = fruitDat)
-plot(proportion_abscised ~ seedpredationrate, data = fruitDat, cex=3,cex.lab=4) + abline(lambdaSP, col="red") + abline(lmodel) 
 
 ###############################################################
 ####### Estimating phylogenetic signal for one variable
 
 # Pagel’s  λ
-model.pgls <- pgls(proportion_abscised ~ 1, data = compDat, lambda = "ML")
+model.pgls <- caper::pgls(proportion_abscised ~ 1, data = compDat, lambda = "ML")
 summary(model.pgls)
+
+m2 <- phylolm::phyloglm(formula = (abscised_seeds/total_seeds) ~ 1, data = fruitDat, phy = phylo)
+pez::communityPGLMM(formula=proportion_abscised ~ 1, data=fruitDat, family="binomial",sp = fruitDat$taxa)
+
+gls(proportion_abscised ~,data=data,correlation=corPagel(value=1,phy=tree,fixed=FALSE),method="ML")
 
 #can look at the likelihood profiles for branch length transformations in PGLS models using pgls.profile
 
@@ -159,14 +143,31 @@ pgls.confint(model.pgls, "lambda")$ci.val
 p_a <- fruitDat$proportion_abscised
 names(p_a) <- fruitDat$taxa
 Kcalc(p_a[phylo$tip.label], phylo)
+
+# lambda
 picante::phylosignal(p_a[phylo$tip.label], phylo, reps = 1000)
 
-phytools::phylosig(x=p_a,tree=phylo, test=TRUE, nsim = 1000)
+# k
+phytools::phylosig(x=p_a,tree=phylo, test=TRUE, nsim = 1000, method="K")
+phytools::phylosig(x=p_a,tree=phylo, test=TRUE, nsim = 1000, method="lambda")
 
 phytools::plotTree(newtree,p_a, showTipLabel=FALSE, showNodeLabel=FALSE, fsize=0.5)
 
-png(file = "../output/plots/prematureFruitDrop/contMap.png",  
+png(file = "../output/plots/prematureFruitDrop/contMap_t.png",  
     width = 2560, # The width of the plot in inches
     height = 1440)
-contMap(phylo, p_a, fsize=1.5, type="fan", lwd=6)
+contMap(phylo, p_a, fsize=2, type="fan", lwd=6)
 dev.off()
+
+ggsave("../output/plots/contMap_t.png", dpi="screen")
+
+##########
+
+p_a <- fruitDat$proportion_abscised
+names(p_a) <- fruitDat$taxa
+
+bci <- fruitDat$bcireproductive_log
+names(bci) <- fruitDat$taxa
+
+phytools::phylosig(x=bci,tree=phylo, test=TRUE, nsim = 1000, method="K")
+phytools::phylosig(x=bci,tree=phylo, test=TRUE, nsim = 1000, method="lambda")
