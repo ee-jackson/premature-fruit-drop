9454a0c6faca30f239f5197d7b6ac49b176d42f7
Eleanor Jackson <eleanor.elizabeth.j@gmail.com>
2021-06-02 17:07:55 +0100

    save to pdf not tiff

--- code/01_plot-proportion-abscised.R
+++ code/01_plot-proportion-abscised.R
@@ -12,14 +12,10 @@ groundhog_day = "2021-04-29"
 groundhog.library("tidyverse", groundhog_day)
 groundhog.library("reshape2", groundhog_day)
 groundhog.library("ggcorrplot", groundhog_day)
-groundhog.library("here", groundhog_day)
 
 # Load data ---------------------------
 
-load(here::here("data", "clean", "fruit_traits.RData"))
-
-# only include sp x year points if there were at least 10 parts found
-fruit_traits <- subset(fruit_traits, sum_parts >= 10)
+fruit_traits <- readRDS(here::here("data", "clean", "fruit_traits.rds"))
 
 # Plot histogram ---------------------------
 
@@ -31,13 +27,13 @@ fruit_traits %>%
 	    geom_histogram(aes(y=..count..), binwidth=.05, colour="black", fill="grey", alpha=.2, size=0.25, boundary=0) +
 		scale_y_continuous(expand= c(0, 0), lim =c(0,45)) +
 		scale_x_continuous(expand= c(0, 0)) +
-		xlab("Mean proportion of seeds prematurely abscised") +
-		ylab("Count of plant species") +
+		xlab("mean proportion of seeds prematurely abscised") +
+		ylab("count of plant species") +
 		theme_classic(base_size = 8) +
 		theme(plot.margin = margin(2, 3, 2, 2, "mm") )
 
-ggsave(here::here("output", "figures", "01_histogram.tiff"),
-    device = "tiff", dpi = 350, width = 80, height = 60, units = "mm")
+ggsave(here::here("output", "figures", "01_histogram.pdf"),
+    device = "pdf", dpi = 600, width = 80, height = 60, units = "mm")
 
 # Plot correlation matrix ---------------------------
 
@@ -52,15 +48,21 @@ fruit_traits %>%
 	 .groups ="keep")%>%
 	ungroup() %>%
 	select(-sp4) %>%
-	rename("Tree height" = height_avg, "Interannual crop\nsize variation" = cvseed, "Overlap in\nfruit production" = cofruit, "Local\nAbundance" = bcireproductive, "Endocarp\ninvestment" = endocarp_investment, "Seed dry mass" = seed_dry, "mean Proportion of\nseeds abscised" = proportion_abscised_m) -> cor_traits
+	rename("Tree height" = height_avg,
+	       "Temporal crop\nsize variation" = cvseed,
+	       "Temporal overlap in\nfruit production" = cofruit,
+	       "Local Abundance\nof conspecifics" = bcireproductive,
+	       "Investment in mechanical\nseed defences" = endocarp_investment,
+	       "Seed mass" = seed_dry,
+	       "Mean proportion of\nseeds abscised" = proportion_abscised_m) -> cor_traits
 
 # plot
 ggcorrplot(cor(cor_traits, use = "pairwise.complete.obs", method="spearman"),
 	p.mat = cor_pmat(cor_traits, use = "pairwise.complete.obs",
 	method = "spearman"), hc.order = TRUE, type = "lower",lab = TRUE,
-	sig.level = 0.05, insig = "pch", pch.cex = 10, lab_size = 3,
-	tl.cex = 7, digits = 2, colors = c("#ca0020", "#f7f7f7", "#0571b0")) +
+	sig.level = 0.05, insig = "pch", pch.cex = 8, lab_size = 2,
+	tl.cex = 7, digits = 2, colors = c("#0571b0", "#f7f7f7", "#ca0020")) +
 	theme(axis.text.x=element_text(angle = 90, hjust = 0.5, vjust = 0.5), legend.position="none")
 
-ggsave(here::here("output", "figures", "s1_correlations.tiff"),
-    device = "tiff", dpi = 350, width = 80, height = 80, units = "mm")
+ggsave(here::here("output", "figures", "s1_correlations.pdf"),
+       device = "pdf", dpi = 600, width = 80, height = 80, units = "mm")
