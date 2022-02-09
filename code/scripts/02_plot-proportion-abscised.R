1612883290f254a2ba9afc3754955a50263ec185
Eleanor Jackson <eleanor.elizabeth.j@gmail.com>
2022-02-09 19:05:15 +0000

    consistent spaces!

--- code/scripts/02_plot-proportion-abscised.R
+++ code/scripts/02_plot-proportion-abscised.R
@@ -22,10 +22,11 @@ fruit_traits %>%
 	select("sp4","year", "proportion_abscised") %>%
 	group_by(sp4) %>%
 	summarise(proportion_abscised_m = mean(proportion_abscised)) %>%
-	ggplot(aes(x=proportion_abscised_m)) +
-    geom_histogram(aes(y=..count..), binwidth=.05, colour="white", fill="grey", size=0.25, boundary=0) +
-		scale_y_continuous(expand= c(0, 0), lim =c(0,45)) +
-		scale_x_continuous(expand= c(0, 0)) +
+	ggplot(aes(x = proportion_abscised_m)) +
+    geom_histogram(aes(y = ..count..), binwidth = .05, colour = "white",
+                   fill = "grey", size = 0.25, boundary = 0) +
+		scale_y_continuous(expand = c(0, 0), lim = c(0,45)) +
+		scale_x_continuous(expand = c(0, 0)) +
 		xlab("Mean proportion of seeds prematurely abscised") +
 		ylab("Count of plant species") +
 		theme_classic(base_size = 8) +
@@ -42,7 +43,7 @@ fruit_traits$bcireproductive_log <- log(fruit_traits$bcireproductive)
 
 # reformat data for ggcorrplot
 fruit_traits %>%
-	group_by(sp4,height_avg,cvseed,cofruit,endocarp_investment,seed_dry,bcireproductive) %>%
+	group_by(sp4, height_avg, cvseed, cofruit, endocarp_investment, seed_dry, bcireproductive) %>%
 	summarise(proportion_abscised_m = mean(proportion_abscised),
 	 .groups ="keep")%>%
 	ungroup() %>%
@@ -61,7 +62,7 @@ ggcorrplot(cor(cor_traits, use = "pairwise.complete.obs", method="spearman"),
 	method = "spearman"), hc.order = TRUE, type = "lower",lab = TRUE,
 	sig.level = 0.05, insig = "pch", pch.cex = 8, lab_size = 2,
 	tl.cex = 7, digits = 2, colors = c("#0571b0", "#f7f7f7", "#ca0020")) +
-	theme(axis.text.x=element_text(angle = 90, hjust = 0.5, vjust = 0.5), legend.position="none")
+	theme(axis.text.x = element_text(angle = 90, hjust = 0.5, vjust = 0.5), legend.position="none")
 
 ggsave(here::here("output", "figures", "s1_correlations.pdf"),
        device = "pdf", dpi = 600, width = 80, height = 80, units = "mm")
